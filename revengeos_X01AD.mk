c66e0fef84f043facc3ec3d7ac6789b8d5245e83
Puneet Gautam <89017901+kryptoniteX@users.noreply.github.com>
2021-09-09 16:01:15 +0530

    X01AD:update to redfin september fp

--- revengeos_X01AD.mk
+++ revengeos_X01AD.mk
@@ -37,9 +37,9 @@ BOARD_VENDOR := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_MODEL=ASUS_X01AD \
-    PRIVATE_BUILD_DISC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
+    PRIVATE_BUILD_DISC="redfin-user 11 redfin:11/RQ3A.210905.001 7511028 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
